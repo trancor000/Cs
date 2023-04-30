b6aec0f97879758af6c548955e0d17a1a1ca8b9f
trancor000 <83535306+trancor000@users.noreply.github.com>
2023-04-30 15:36:39 -0700

    Add files via upload

---
+++ CodingSkills.cs
@@ -0,0 +1,40 @@
+﻿
+using System;
+
+namespace CodingSkills
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Skills skills1 = new Skills("Nathan", 24, "Junior");
+            Skills skills2 = new Skills("Matthew", 30, "Senior");
+            Skills skills3 = new Skills("Clarice", 22, "Senior");
+
+            skills1.Review();
+            skills2.Review();
+            skills3.Review();
+
+            Console.ReadKey();
+        }
+    }
+
+    class Skills
+    {
+        String name;
+        int age;
+        String developerLevel;
+
+        public Skills(String name, int age, String developerLevel)
+        {
+            this.name = name;
+            this.age = age;
+            this.developerLevel = developerLevel;
+        }
+
+        public void Review()
+        {
+            Console.WriteLine($"Your name is {name}, your age is {age}, and your developer level is {developerLevel}.");
+        }
+    }
+}
