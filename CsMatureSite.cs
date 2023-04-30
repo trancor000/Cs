6a9f128acc03276a2f26c887fc34feebcaf0c2a0
trancor000 <83535306+trancor000@users.noreply.github.com>
2023-04-30 14:31:18 -0700

    Add files via upload

---
+++ CsMatureSite.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+
+namespace CsMatureSite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello and welcome to this Mature Site!");
+            Console.WriteLine("This site is only for 18+. Please state your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            if (age >= 18 ) 
+            {
+                Console.WriteLine("You are now entering this mature site.");
+            }
+            else
+            {
+                Console.WriteLine("You are not old enough to enter this mature site.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
