48b057fcc28c26a555635aee932c291c4ea5449e
trancor000 <83535306+trancor000@users.noreply.github.com>
2023-04-30 14:35:54 -0700

    Add files via upload

---
+++ RPS.cs
@@ -0,0 +1,114 @@
+﻿
+using System;
+
+namespace CsRPS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declared and instantiated variables outside game method
+            Random random = new Random();
+            bool playAgain = true;
+            String player;
+            String computer;
+            String answer;
+
+            while (playAgain)
+            {
+                player = ""; //set as empty string so that it correlates correctly with "startover" playagain
+                computer = "";
+                answer = "";
+
+                while (player != "ROCK" && player != "PAPER" && player != "SCISSORS") //while player's pick does not equal an option
+                { //nested "while" loop
+                  //used in case player enters an option that's not rock, paper, or scissors
+                    Console.Write("Enter ROCK, PAPER, or SCISSORS: ");
+                    player = Console.ReadLine();
+                    player = player.ToUpper(); //default to uppercase for sensitivity
+                }
+
+                switch (random.Next(1, 4)) //"4" is exclusive. Valid for 3 options
+                {//computer choosing randomly between the 3 options
+                    case 1:
+                        computer = "ROCK";
+                        break;
+                    case 2:
+                        computer = "PAPER";
+                        break;
+                    case 3:
+                        computer = "SCISSORS";
+                        break;
+                }
+
+                Console.WriteLine("Player: " + player);
+                Console.WriteLine("Computer: " + computer); //used to portray the choices player picked and computer picked
+
+                //deciding who wins
+                switch (player) //player against the picks
+                {
+                    case "ROCK": //player picks rock
+                        if (computer == "ROCK")
+                        {
+                            Console.WriteLine("It's a draw!");
+                        }
+                        else if (computer == "PAPER")
+                        {
+                            Console.WriteLine("You lose!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("You win!");
+                        }
+                        break;
+                    case "PAPER":
+                        if (computer == "ROCK")
+                        {
+                            Console.WriteLine("You win!");
+                        }
+                        else if (computer == "PAPER")
+                        {
+                            Console.WriteLine("It's a draw!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("You lose!");
+                        }
+                        break;
+                    case "SCISSORS":
+                        if (computer == "ROCK")
+                        {
+                            Console.WriteLine("You lose!");
+                        }
+                        else if (computer == "PAPER")
+                        {
+                            Console.WriteLine("You win!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("It's a draw!");
+                        }
+                        break;
+                }
+
+                Console.Write("Would you like to play again (Y/N): "); //inside nested while to determine if playagain
+                answer = Console.ReadLine();
+                answer = answer.ToUpper();
+
+                if (answer == "Y")
+                {
+                    playAgain = true;
+                }
+                else
+                {
+                    playAgain = false;
+                }
+
+            }
+
+            Console.WriteLine("Thanks for playing!");
+
+            Console.ReadKey();
+        }
+    }
+}
