a52847d00d24eafac50f36052e4bf7b151727cbe
trancor000 <83535306+trancor000@users.noreply.github.com>
2023-04-30 23:32:22 -0700

    Add files via upload

---
+++ tempLimit.cs
@@ -0,0 +1,45 @@
+﻿
+using System;
+
+namespace tempLimit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Limit limit = new Limit(90);
+
+            limit.Temp = 1000;
+
+            Console.WriteLine(limit.Temp);
+
+            Console.ReadKey();
+        }
+    }
+
+    class Limit
+    {
+        private int temp;
+
+        public Limit(int temperature) 
+        {
+            Temp = temp;
+        }
+
+        public int Temp
+        {
+            get { return temp; }
+            set
+            {
+                if (value > 100)
+                {
+                    temp = 100;
+                }
+                else
+                {
+                    temp = value;
+                }
+            }
+        }
+    }
+}
